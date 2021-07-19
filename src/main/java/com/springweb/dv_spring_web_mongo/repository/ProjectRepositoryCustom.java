3cec942617f1c151ae48f1f6ed1f2e6f2e328b49
Alex F <flegontov007@gmail.com>
2021-07-19 16:44:56 +0300

    Added repository and service classes

---
+++ src/main/java/com/springweb/dv_spring_web_mongo/repository/ProjectRepositoryCustom.java
@@ -0,0 +1,43 @@
+package com.springweb.dv_spring_web_mongo.repository;
+
+import com.springweb.dv_spring_web_mongo.model.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProjectRepositoryCustom {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<Project> getAllProjects(){
+        List<Project> list = mongoTemplate.findAll(Project.class);
+        return list;
+    }
+
+    public Project getProjectById(String id){
+        Query search = new Query(Criteria.where("_id").is(id)) ;
+        Project project  = mongoTemplate.findOne(search, Project.class);
+        return project;
+    }
+
+    public void addNewProject(Project project){
+        mongoTemplate.save(project);
+    }
+
+    public void changeProjectName(Project project, String id){
+        Query search = new Query(Criteria.where("_id").is(id));
+        mongoTemplate.updateFirst(search, Update.update("projectName",project.getProjectName()),Project.class);
+    }
+
+
+    public void deleteProject( String id){
+        Query search = new Query(Criteria.where("_id").is(id));
+        mongoTemplate.remove(search,Project.class);
+    }
+}
