81e4f624cb9767de40019739ea86565eba8dbe7f
Alex F <flegontov007@gmail.com>
2021-07-21 22:24:05 +0300

    Using lombok library in Project model

--- src/main/java/com/springweb/dv_spring_web_mongo/model/Project.java
+++ src/main/java/com/springweb/dv_spring_web_mongo/model/Project.java
@@ -3,6 +3,9 @@ package com.springweb.dv_spring_web_mongo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.springweb.dv_spring_web_mongo.DTO.ProjectDTO;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,37 +13,15 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.annotation.processing.Generated;
 
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Document(collection = "LearningMongo")
 public class Project {
-
-    public Project() {}
 
     @Id
     private String id;
 
     private String projectName;
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    @Override
-    public String toString() {
-        return "project{" +
-                "id=" + id +
-                ", projectName='" + projectName + '\'' +
-                '}';
-    }
 }
