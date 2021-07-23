4e283b1e51005f8d0af7a21319db43ee2037f190
Alex F <flegontov007@gmail.com>
2021-07-23 11:22:47 +0300

    # Conflicts: #	src/main/java/com/springweb/dv_spring_web_mongo/controller/ProjectController.java #	src/main/java/com/springweb/dv_spring_web_mongo/model/Project.java #	src/main/java/com/springweb/dv_spring_web_mongo/repository/ProjectRepositoryCustom.java #	src/main/java/com/springweb/dv_spring_web_mongo/service/ProjectService.java

--- src/main/java/com/springweb/dv_spring_web_mongo/model/Project.java
+++ src/main/java/com/springweb/dv_spring_web_mongo/model/Project.java
@@ -1,43 +1,29 @@
 package com.springweb.dv_spring_web_mongo.model;
 
 
-import com.springweb.dv_spring_web_mongo.dto.ProjectDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.springweb.dv_spring_web_mongo.DTO.ProjectDTO;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import javax.annotation.processing.Generated;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Document(collection = "LearningMongo")
 public class Project {
-
-    public Project(String id, String projectName) {
-        this.id = id;
-        this.projectName = projectName;
-    }
 
     @Id
     private String id;
 
     private String projectName;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public ProjectDTO convertToDTO() {
-        return new ProjectDTO(this.id, this.projectName);
-    }
 }
