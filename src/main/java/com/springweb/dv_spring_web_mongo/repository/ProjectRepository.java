d8c8342cbc68ed1bac089e0da076b48482b870d9
Alex F <flegontov007@gmail.com>
2021-07-26 12:31:09 +0300

    Added new logic in ProjectService

--- src/main/java/com/springweb/dv_spring_web_mongo/repository/ProjectRepository.java
+++ src/main/java/com/springweb/dv_spring_web_mongo/repository/ProjectRepository.java
@@ -3,6 +3,8 @@ package com.springweb.dv_spring_web_mongo.repository;
 import com.springweb.dv_spring_web_mongo.model.Project;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProjectRepository extends MongoRepository<Project, String> {
+import java.util.List;
 
+public interface ProjectRepository extends MongoRepository<Project, String> {
+    List<Project> findAllByProjectName(String projectName);
 }
